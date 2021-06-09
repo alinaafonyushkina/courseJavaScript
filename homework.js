e56fe68bcfe0bfd561e9bd50be6d95cb86cf4517
alinaafonyushkina <80514426+alinaafonyushkina@users.noreply.github.com>
2021-06-09 08:58:41 +0300

    Домашняя работа - 4

--- homework.js
+++ homework.js
@@ -1,99 +1,79 @@
 "use strict"
 
+
 //Задание 1
 
+console.log('Ответ задача 1');
 
-//      Создаем массив от 1 до 100
-var arr = [];
-var end = 1;
-while (end < 100) {
-    end++;
-    arr.push(end);
+var max = 999;
+var digit = {
+    number: Math.round(Math.random() * max), /*запрос у пользователя +prompt('Введите число от 0 до 999'),*/
+    units: 0,
+    tens: 0,
+    hundreds: 0,
+};
+if (digit.number <= 9) {
+    digit.units = digit.number;
+} else if (digit.number <= 999) {
+    digit.units = Math.floor(digit.number % 10);
+    digit.tens = Math.floor(digit.number / 10 % 10);
+    digit.hundreds = Math.floor(digit.number / 100 % 10);
+} else {
+    digit.number = 0;
+    console.log('Вы ввели число за диапазоном 0 - 999');
 }
-//      Удаляем все четные цифры
-var a = 2;
-var b = 2;
-while (a <= 100) {
-    a += b;
-    arr.splice(arr.indexOf(a), 1);
-}
-//      Удаляет все цифры которые деляться на нечетные до 10
-a = 4;
-b = 3;
-while (a < 100) {
-    if (a % b === 0 && arr.indexOf(a) !== -1) {
-        arr.splice(arr.indexOf(a), 1);
-    } else {
-        a++;
-    }
-}
-
-a = 6;
-b = 5;
-while (a < 100) {
-    if (a % b === 0 && arr.indexOf(a) !== -1) {
-        arr.splice(arr.indexOf(a), 1);
-    } else {
-        a++;
-    }
-}
-
-a = 8;
-b = 7;
-while (a < 100) {
-    if (a % b === 0 && arr.indexOf(a) !== -1) {
-        arr.splice(arr.indexOf(a), 1);
-    } else {
-        a++;
-    }
-}
-
-console.log(arr);
-
 
 //Задание 2
 
-let basket = [
+//Функция добавления товара в корзину
+function addProductBasket(arr, name, price, quantity) {
+    arr.push({
+        product: name,
+        price: price,
+        quantity: quantity,
+        total: totalSum
+    });
+}
+
+function totalSum() {
+    return this.price * this.quantity;
+}
+
+var basket = [
     {
-        product: "pen",
-        price: getNumber(50, 100)
+        product: 'shirt',
+        price: 100,
+        quantity: 2,
+        total: totalSum
     },
     {
-        product: "pencil",
-        price: getNumber(50, 100)
+        product: 'shorts',
+        price: 200,
+        quantity: 1,
+        total: totalSum
     },
     {
-        product: "file",
-        price: getNumber(50, 100)
+        product: 'dress',
+        price: 300,
+        quantity: 3,
+        total: totalSum
     },
     {
-        product: "paper",
-        price: getNumber(50, 100)
+        product: 'socks',
+        price: 150,
+        quantity: 5,
+        total: totalSum
+    },
+]
+
+function countBasketPrice(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i].total();
     }
-];
-let basketPrice = 0;
-for (let prod of basket) {
-    basketPrice += prod.price;
-    console.log("Товар " + prod.product + " стоит: " + prod.price);
+    return sum;
 }
 
-console.log("Стоимость корзины: " + basketPrice + " руб.");
 
-//Задание 4
 
-var i;
-for (i = 0; i <= 9; i++) {
-    console.log(i);
-}
-
-//Не знаю как без console.log  вывести... Чтобы пусто было.
-
-//Задание 5
-
-var arr = [];
-var end = 0;
-while (end < 20) {
-    end++;
-    arr.push('x');
-    console.log(arr);
-}
+console.log('Общая стоимость: ' + countBasketPrice(basket));
